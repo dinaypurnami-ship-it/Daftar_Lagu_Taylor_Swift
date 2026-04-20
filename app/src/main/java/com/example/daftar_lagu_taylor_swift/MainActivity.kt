25f30b3bb6ff6b5b1266e0143bec36e11551664d
DinaAyu <dinaypurnami@gmail.com>
2026-04-20 15:29:53 +0800

    Commit navigasi antar halaman (Intent) dan validasi input.

--- app/src/main/java/com/example/daftar_lagu_taylor_swift/MainActivity.kt
+++ app/src/main/java/com/example/daftar_lagu_taylor_swift/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.daftar_lagu_taylor_swift
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Hubungkan variabel dengan ID yang ada di XML tadi
+        val etSearch = findViewById<EditText>(R.id.etSearch)
+        val btnPlay = findViewById<Button>(R.id.btnPlay)
+
+        btnPlay.setOnClickListener {
+            val inputLagu = etSearch.text.toString()
+
+            if (inputLagu.isEmpty()) {
+                etSearch.error = "Dina, tulis judul lagu Taylor favoritmu dulu ya!"
+                Toast.makeText(this, "Input tidak boleh kosong!", Toast.LENGTH_SHORT).show()
+            } else {
+                // Baris DetailActivity di bawah ini akan merah sampai langkah #2 selesai
+                val intent = Intent(this, DetailActivity::class.java)
+                intent.putExtra("NAMA_LAGU", inputLagu)
+                startActivity(intent)
+            }
         }
     }
 }
